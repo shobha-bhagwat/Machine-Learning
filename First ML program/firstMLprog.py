41f31b38233ba4de6b1bc97986c51819075689dc
Shobha Bhagwat <shobha_bhagwat@yahoo.co.in>
2018-11-12 15:14:34 +0530

    ML program
    ML program to identify best model for classification of Iris dataset

--- First ML program/firstMLprog.py
+++ First ML program/firstMLprog.py
@@ -74,33 +74,3 @@ predictions = svc.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
